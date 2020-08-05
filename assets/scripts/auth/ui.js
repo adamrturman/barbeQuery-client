54dbfb75df69ff3ced7d9f6c3f213a89594a3d0e
adamrturman <adamrturman@gmail.com>
2020-08-05 13:29:22 -0500

    Sign In
    Built and tested sign in with curl script and web application

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -10,8 +10,18 @@ const signUpFailure = function (response) {
   $('#message').text('You did not sign up!')
 }
 
+const signInSuccess = function (response) {
+  $('#message').text('You are signed in!')
+  $('form').trigger('reset')
+}
+
+const signInFailure = function (response) {
+  $('#message').text('You did not sign in. Try again!')
+}
 
 module.exports = {
   signUpSuccess,
-  signUpFailure
+  signUpFailure,
+  signInSuccess,
+  signInFailure
 }
