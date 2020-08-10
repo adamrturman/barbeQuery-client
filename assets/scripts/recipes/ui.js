6f9a457b59c03062607acef0dac3e48ff4172214
adamrturman <adamrturman@gmail.com>
2020-08-10 17:28:22 -0500

    Removed recipe id from template

--- assets/scripts/recipes/ui.js
+++ assets/scripts/recipes/ui.js
@@ -31,7 +31,7 @@ const deleteRecipeFailure = (id) => {
 const updateRecipesSuccess = (id) => {
   $('#message-2').text('You updated a recipe!')
   $('form').trigger('reset')
-  //$('#update-modal').modal('hide')
+  $('#update-modal').modal('hide')
   $('.modal-backdrop').remove()
 }
 
